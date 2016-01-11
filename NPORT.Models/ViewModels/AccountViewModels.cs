e8e5923fae1907353ea128504c64cee7e1b8d953
Богдан <bogdan.gork@gmail.com>
2016-01-11 10:48:39 +0200

    123

--- NPORT.Models/ViewModels/AccountViewModels.cs
+++ NPORT.Models/ViewModels/AccountViewModels.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel;
-using System.Web.Mvc;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel;
+//using System.Web.Mvc;
 
-namespace NPORT.Models.ViewModels
-{
+//namespace NPORT.Models.ViewModels
+//{
 
-    public class LoginViewModel
-    {
-        [Required]
-        [Display(Name = "User name (Email)")]
-        public string Login { get; set; }
+//    public class LoginViewModel
+//    {
+//        [Required]
+//        [Display(Name = "User name (Email)")]
+//        public string Login { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Пароль")]
-        public string Password { get; set; }
+//        [Required]
+//        [DataType(DataType.Password)]
+//        [Display(Name = "Пароль")]
+//        public string Password { get; set; }
 
-        [Display(Name = "Запомнить меня")]
-        public bool RememberMe { get; set; }
+//        [Display(Name = "Запомнить меня")]
+//        public bool RememberMe { get; set; }
         
-    }
-}
+//    }
+//}
