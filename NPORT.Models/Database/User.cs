6a00661f805aa3f84fd91a179ed6c770c6e2db13
Богдан <bogdan.gork@gmail.com>
2016-01-11 10:30:26 +0200

    entity framework

--- NPORT.Models/Database/User.cs
+++ NPORT.Models/Database/User.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
 
 namespace NPORT.Models.Database
 {
