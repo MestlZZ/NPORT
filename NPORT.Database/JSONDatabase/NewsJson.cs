008fbb998db8773428b3c7cd3cdb858ac230bdee
Богдан <bogdan.gork@gmail.com>
2016-01-11 12:10:11 +0200

    поиск по логину и паролю

---
+++ NPORT.Database/JSONDatabase/NewsJson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using NPORT.Models;
+using Newtonsoft.Json;
+
+namespace NPORT.Database.JSONDatabase
+{
+    public static class NewsJson
+    {
+        private static string Path = HttpContext.Current.Server.MapPath( "/App_Data/NewsDatabase.json" );
+
+        public static void AddNews( Models.Database.News newNews )
+        {
+            var json = JsonSerializer.Create();
+            StreamWriter text = new StreamWriter(Path);
+            json.Serialize( text, newNews );
+            text.Close();
+        }
+    }
+}
