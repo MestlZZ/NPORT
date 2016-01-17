3b9a5dc2979840402866cd3ded670f3191ace760
Богдан <bogdan.gork@gmail.com>
2016-01-17 13:50:53 +0200

    last fix

--- NPORT.MVC/Controllers/HomeController.cs
+++ NPORT.MVC/Controllers/HomeController.cs
@@ -28,55 +28,5 @@ namespace NPORT.Controllers
 
             return View();
         }
-        
-        public ActionResult AddNews()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult AddNews(NPORT.Models.Database.News news)
-        {
-            if (ModelState.IsValid)
-            {
-                news.AuthorId = User.Identity.GetUserId();
-                NPORT.Database.JSONDatabase.NewsJson.Add( news );
-                return RedirectToAction("Index");
-            }
-            return View( news );
-        }
-
-        [HttpGet]
-        public ActionResult Detailed( string NewsId )
-        {
-            ViewBag.Id = NewsId;
-            return View();
-        }
-        [HttpGet]
-        public ActionResult Edit (string NewsId)
-        {
-            var model = NPORT.Database.JSONDatabase.NewsJson.Find(NewsId);
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult Edit(NPORT.Models.Database.News news)
-        {
-            if (ModelState.IsValid)
-            {
-                NPORT.Database.JSONDatabase.NewsJson.Edit(news);
-                return RedirectToAction("Index");
-            }
-            return View(news);
-        }        
-
-        [HttpGet]
-        public ActionResult Remove(string NewsId)
-        {
-            NPORT.Database.JSONDatabase.NewsJson.Remove(NewsId);
-            return View();
-            //return RedirectToAction("Index");
-        }
     }
 }
---
+++ NPORT.MVC/Controllers/NewsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Web.UI;
+using System.ComponentModel.DataAnnotations;
+namespace NPORT.Controllers
+{
+    public class NewsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult AddNews()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput( false )]
+        public ActionResult AddNews( NPORT.Models.Database.News news )
+        {
+            if (ModelState.IsValid)
+            {
+                news.AuthorId = User.Identity.GetUserId();
+                NPORT.Database.JSONDatabase.NewsJson.Add( news );
+                return RedirectToAction( "Index" );
+            }
+            return View( news );
+        }
+
+        [HttpGet]
+        public ActionResult Detailed( string NewsId )
+        {
+            ViewBag.Id = NewsId;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit( string NewsId )
+        {
+            var model = NPORT.Database.JSONDatabase.NewsJson.Find(NewsId);
+            return View( model );
+        }
+
+        [HttpPost]
+        [ValidateInput( false )]
+        public ActionResult Edit( NPORT.Models.Database.News news )
+        {
+            if (ModelState.IsValid)
+            {
+                NPORT.Database.JSONDatabase.NewsJson.Edit( news );
+                return RedirectToAction( "Index" );
+            }
+            return View( news );
+        }
+
+        [HttpGet]
+        public ActionResult Remove( string NewsId )
+        {
+            NPORT.Database.JSONDatabase.NewsJson.Remove( NewsId );
+            return View();
+            //return RedirectToAction("Index");
+        }
+    }
+}
