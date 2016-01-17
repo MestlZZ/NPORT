3a3e40ecf0eed4875fe8778274dd499bf279aae9
Богдан <bogdan.gork@gmail.com>
2016-01-17 14:25:52 +0200

    routes

--- NPORT.MVC/App_Start/RouteConfig.cs
+++ NPORT.MVC/App_Start/RouteConfig.cs
@@ -14,15 +14,81 @@ namespace NPORT
             routes.IgnoreRoute( "{resource}.axd/{*pathInfo}" );
 
             routes.MapRoute(
-               name: "News",
+               name: "News Details",
                url: "news/item/{id}",
-               defaults: new { controller = "Home", action = "Detailed", id = UrlParameter.Optional, }
+               defaults: new { controller = "News", action = "Detailed", id = UrlParameter.Optional }
            );
 
             routes.MapRoute(
-               name: "Edit",
+               name: "User Login",
+               url: "login",
+               defaults: new { controller = "Account", action = "Login" }
+           );
+
+            routes.MapRoute(
+               name: "About us",
+               url: "about",
+               defaults: new { controller = "Home", action = "About" }
+           );
+
+            routes.MapRoute(
+               name: "Contact info",
+               url: "info",
+               defaults: new { controller = "Home", action = "Contact" }
+           );
+
+            routes.MapRoute(
+               name: "User Register",
+               url: "register",
+               defaults: new { controller = "Account", action = "Register" }
+           );
+
+            routes.MapRoute(
+               name: "User LogOff",
+               url: "logoff",
+               defaults: new { controller = "Account", action = "Register" }
+           );
+
+            routes.MapRoute(
+               name: "News Edit",
                url: "news/edit/{id}",
-               defaults: new { controller = "Home", action = "Edit", id = UrlParameter.Optional, }
+               defaults: new { controller = "News", action = "Edit", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "News Remove",
+               url: "news/remove",
+               defaults: new { controller = "News", action = "Remove" }
+           );
+
+            routes.MapRoute(
+               name: "News Add",
+               url: "news/add",
+               defaults: new { controller = "News", action = "AddNews" }
+           );
+
+            routes.MapRoute(
+               name: "User panel",
+               url: "user/",
+               defaults: new { controller = "User", action = "Index" }
+           );
+
+            routes.MapRoute(
+               name: "User list",
+               url: "users",
+               defaults: new { controller = "User", action = "UserList" }
+           );
+
+            routes.MapRoute(
+               name: "User details",
+               url: "user/info/{id}",
+               defaults: new { controller = "User", action = "Details", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "User remove",
+               url: "user/remove",
+               defaults: new { controller = "User", action = "Remove", id = UrlParameter.Optional }
            );
 
             routes.MapRoute(
---
+++ NPORT.MVC/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Web.UI;
+using System.ComponentModel.DataAnnotations;
+using NPORT.Models;
+
+namespace NPORT.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UserList()
+        {
+            return View( Database.XMLDatabase.Users.GetList() );
+        }        
+
+        public ActionResult Details( string Id )
+        {
+            return View( Database.XMLDatabase.Users.Find(Id) );
+        }
+
+        [HttpPost]
+        public ActionResult Details( string Id, string Role )
+        {
+            var user = Database.XMLDatabase.Users.Find(Id);
+            user.Role = Convert.ToInt32( Role );
+            CustomUserStore store = new CustomUserStore();
+
+            store.Update( user );
+
+            return RedirectToAction("UserList");
+        }
+
+        public ActionResult Remove( string Id )
+        {
+            CustomUserStore store = new CustomUserStore();
+
+            var user = store.FindById(Id);
+
+            store.Delete( user );
+
+            var news = Database.JSONDatabase.NewsJson.GetList();
+
+            foreach(var item in news)
+            {
+                if (item.AuthorId == Id)
+                    Database.JSONDatabase.NewsJson.Remove( item.Id );
+            }
+
+            return View();
+        }
+    }
+}
