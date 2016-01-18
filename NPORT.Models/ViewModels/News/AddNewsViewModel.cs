c97faa1b59045781e590607e2c91ff41a506e23c
Богдан <bogdan.gork@gmail.com>
2016-01-18 09:40:56 +0200

    566

---
+++ NPORT.Models/ViewModels/News/AddNewsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using NPORT.Models.ViewModels.Shared;
+using NPORT.Models.Database;
+
+namespace NPORT.Models.ViewModels.News
+{
+    public class AddNewsViewModel : _TitleViewModel
+    {
+        public bool Access_AddNews { get; set; }
+
+        public List<Role> Roles { get; set; }
+
+        [HiddenInput( DisplayValue = false )]
+        public string Id { get; set; }
+
+        [Required]
+        public string NewsTitle { get; set; }
+
+        [Required]
+        public string ShortInfo { get; set; }
+
+        [Required]
+        public int VisibleRange { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [HiddenInput( DisplayValue = false )]
+        public string AuthorId { get; set; }
+
+        [HiddenInput( DisplayValue = false )]
+        public string Date { get; set; }
+    }
+}
