1da99231e17282ec2f0e87aefc6891440ebd2fc5
Богдан <bogdan.gork@gmail.com>
2016-01-18 11:01:14 +0200

    fix

--- NPORT.Models/ViewModels/News/DetailedViewModel.cs
+++ NPORT.Models/ViewModels/News/DetailedViewModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NPORT.Models.ViewModels.Shared;
 
 namespace NPORT.Models.ViewModels.News
 {
-    class DetailedViewModel
+    public class DetailedViewModel : _TitleViewModel
     {
+        public Database.News CurrentNews{ get; set; }
+
+        public int CurrentUserRoleId { get; set; }
+
+        public string CurrentNewsAuthorName { get; set; }
+
+        public bool CurrentUserAccess_EditNews { get; set; }
+
+        public bool CurrentUserAccess_RemoveNews { get; set; }
+
+        public string CurrentUserId { get; set; }
+
+        public List<Database.Comment> CommentsList { get; set; }
+
+        public List<string> UserListForComments { get; set; }
     }
 }
